f42cfe980f699d211a1dab3bb44898adc7b49065
William Hearn <sylus1984@gmail.com>
2021-04-04 16:28:26 -0400

    feat(initial): Baseline

---
+++ README.md
@@ -0,0 +1,3 @@
+# Terraform for Azure Managed Database MSSQL Server
+
+Creates MSSQL Server for use with the Azure Managed Database for MSSQL.
---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "id" {
+  value = azurerm_mssql_server.mssql.id
+}
+
+output "name" {
+  value = azurerm_mssql_server.mssql.name
+}
+
+output "identity_tenant_id" {
+  value = azurerm_mssql_server.mssql.identity[0].tenant_id
+}
+
+output "identity_object_id" {
+  value = azurerm_mssql_server.mssql.identity[0].principal_id
+}
+
+# Part of a hack for module-to-module dependencies.
+# https://github.com/hashicorp/terraform/issues/1178#issuecomment-449158607
+# and
+# https://github.com/hashicorp/terraform/issues/1178#issuecomment-473091030
+output "depended_on" {
+  value = "${null_resource.dependency_setter.id}-${timestamp()}"
+}
