526d143b224e1df2fce0c09b1bb289dd65f3d76e
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-09-07 17:34:53 +0000

    output update

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "id" {
-  value = azurerm_mssql_server.mssql.id
+  value = azurerm_mssql_server.this.id
 }
 
 output "name" {
-  value = azurerm_mssql_server.mssql.name
+  value = azurerm_mssql_server.this.name
 }
 
 output "identity_tenant_id" {
-  value = azurerm_mssql_server.mssql.identity[0].tenant_id
+  value = azurerm_mssql_server.this.identity[0].tenant_id
 }
 
 output "identity_object_id" {
-  value = azurerm_mssql_server.mssql.identity[0].principal_id
+  value = azurerm_mssql_server.this.identity[0].principal_id
 }
 
 output "sa_primary_blob_endpoint" {
-  value = azurerm_mssql_server_extended_auditing_policy.mssql.storage_endpoint
+  value = azurerm_mssql_server_extended_auditing_policy.this.storage_endpoint
 }
 
 output "sa_primary_access_key" {
-  value = var.keyvault_enable ? null : azurerm_mssql_server_extended_auditing_policy.mssql.storage_account_access_key
+  value = var.kv_enable ? null : azurerm_mssql_server_extended_auditing_policy.this.storage_account_access_key
 }
 
 output "assessment_id" {
-  value = azurerm_mssql_server_vulnerability_assessment.mssql.id
+  value = azurerm_mssql_server_vulnerability_assessment.this.id
 }
 
 output "firewall" {
